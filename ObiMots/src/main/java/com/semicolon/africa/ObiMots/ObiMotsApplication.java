f3a63bbde5442e8ed0d624f69e455687d4e9f589
chi-touch <gloebudav@gmail.com>
2024-09-01 00:32:03 +0100

    creating  a project

---
+++ ObiMots/src/main/java/com/semicolon/africa/ObiMots/ObiMotsApplication.java
@@ -0,0 +1,13 @@
+package com.semicolon.africa.ObiMots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObiMotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObiMotsApplication.class, args);
+	}
+
+}
---
+++ ObiMots/src/test/java/com/semicolon/africa/ObiMots/ObiMotsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semicolon.africa.ObiMots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObiMotsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
